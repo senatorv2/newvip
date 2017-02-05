26a783626fcc7c6d631d1eab39c7c759cf5e7436
senatorv2 <shpan@hi2.in>
2017-02-06 01:24:59 +0330

    Update admin.lua

--- plugins/admin.lua
+++ plugins/admin.lua
@@ -1,8 +1,8 @@
 ﻿local function run(msg, matches)
 if is_sudo then
-if matches[1] == 'import' then
+if matches[1] == 'اضافه کردن' then
 tg.importChatInviteLink('https://telegram.me/joinchat/'..matches[2])
-elseif matches[1] == 'setbotname' then
+elseif matches[1] == 'تغییر اسم' then
 if matches[3] then
 text = "نام ربات به "..matches[2]..matches[3].." تغییر یافت."
 tg.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html')
@@ -12,14 +12,14 @@ text = "نام ربات به "..matches[2].." تغییر یافت."
 tg.sendMessage(msg.chat_id_, 0, 1, text, 1, 'html')
 tg.changeName(matches[2], '')
 end
-elseif matches[1] == 'setbotusername' then
+elseif matches[1] == 'تغییر یوزرنیم' then
 tg.changeUsername(matches[2])
-elseif matches[1] == 'rembotusername' then
+elseif matches[1] == 'حذف یوزرنیم' then
 tg.changeUsername('')
-elseif matches[1] == 'setbotphoto' then
+elseif matches[1] == 'تغییر عکس' then
 tg.sendMessage(msg.chat_id_, 0, 1, 'لطفا تصویر را ارسال کنید.', 1, 'html')
 redis:set('botphoto','yes')
-elseif matches[1] == 'photo' and redis:get('botphoto') then
+elseif matches[1] == 'عکس' and redis:get('botphoto') then
 local file = 'bot/photos/bot.jpg'
 print('File downloaded to:', msg.content_.photo_.sizes_[0].photo_.path_)
 os.rename(msg.content_.photo_.sizes_[0].photo_.path_, file)
@@ -42,18 +42,17 @@ end
 end
 return {
   patterns = {
-    "^[#!/](import) https://telegram.me/joinchat/(.*)$",
-	"^[#!/](setbotname) (.*) (.*)$",
-	"^[#!/](setbotname) (.*)$",
-	"^[#!/](setbotusername) (.*)$",
-	"^[#!/](rembotusername)$",
-	"^[#!/](setbotphoto)$",
+    "^اضافه کردن https://telegram.me/joinchat/(.*)$",
+	"^تغییر اسم (.*) (.*)$",
+	"^تغییر اسم (.*)$",
+	"^تغییر یوزرنیم (.*)$",
+	"^حذف یوزرنیم $",
+	"^تنظیم عکس$",
     "^[#!/](markread) (.*)$",
-	"^!!!edit:[#!/](markread) (.*)$",
-	"^!!!(photo):$",
+	"^!!!ویرایش:[#!/](markread) (.*)$",
+	"^!!!عکس:$",
   },
   run = run
 }
 
---@mafia_cli
---https://github.com/mafia-007
+--@Lv_t_m
