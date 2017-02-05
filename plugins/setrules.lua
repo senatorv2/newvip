da213bfde5c0013eb74fb19504666b30bf88c58d
senatorv2 <shpan@hi2.in>
2017-02-06 01:08:03 +0330

    Update setrules.lua

--- plugins/setrules.lua
+++ plugins/setrules.lua
@@ -1,20 +1,20 @@
 local function run(msg, matches)
 local group = load_data('bot/group.json')
 local addgroup = group[tostring(msg.chat_id)]
-if matches[1] == 'setrules' and is_owner(msg) or is_momod(msg) and groupa then
+if matches[1] == 'تنظیم قوانین' and is_owner(msg) or is_momod(msg) and groupa then
 redis:set('rules'..msg.chat_id_,matches[2])
-tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Rules Saved</b>', 1, 'html')
+tg.sendMessage(msg.chat_id_, 0, 1, '<b>قوانین ذخیره شد</b>', 1, 'html')
 end
-if matches[1] == 'rules' and is_owner(msg) or is_momod(msg) and addgroup then
+if matches[1] == 'قوانین' and is_owner(msg) or is_momod(msg) and addgroup then
 rules1 = redis:get('rules'..msg.chat_id_)
-tg.sendMessage(msg.chat_id_, 0, 1, '<b>Group Rules :</b>\n'..rules1 , 1, 'html')
+tg.sendMessage(msg.chat_id_, 0, 1, '<b>قوانین گروه:</b>\n'..rules1 , 1, 'html')
 end
 end
 return {
   patterns = {
-"^[/#!](setrules) (.*)$",
-"^[/#!](rules)$"
+"^تنظیم قوانین(.*)$",
+"^قوانین$"
  },
   run = run
 }
---@mafia_cli
+--@Lv_t_m
