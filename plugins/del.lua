cb08b51dd01a89aa4c722df2acaaf2a8751d79f2
senatorv2 <shpan@hi2.in>
2017-02-06 01:34:53 +0330

    Update del.lua

--- plugins/del.lua
+++ plugins/del.lua
@@ -1,5 +1,5 @@
 local function run(msg, matches)
-    if matches[1] == ("del")and is_owner(msg) then
+    if matches[1] == ("حذف")and is_owner(msg) then
           tg.deleteMessages(msg.chat_id_, {[0] = msg.reply_to_message_id_})
       end
 end
@@ -7,9 +7,9 @@ return {
   patterns = {
 
 
-	"^[#!/](del)$"
+	"^حذف$"
 
   },
   run = run
 }
---channel : mafia_cli
+--channel : senator_tem
